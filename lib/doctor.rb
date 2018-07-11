4eb4d38de56ca10ac8514d49f60cc99cf81f0c63
Jamie Brown <jmbrowncow@gmail.com>
2018-07-11 00:55:32 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,27 @@
+class Doctor
+
+  attr_accessor :name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    self.name= name
+    self.class.all<<self
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select {|a| a.doctor = self}
+  end
+
+  def patients
+    self.appointments.collect {|a| a.patient}
+  end
+end
